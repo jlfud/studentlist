9b13ca8974500a0a049402f2c34140d48c46e58b
jlfudeveloper <69946005+jlfudeveloper@users.noreply.github.com>
2020-09-28 09:29:49 -0700

    initial commit

---
+++ studentlist.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+struct Student{
+  char firstname[10];
+  char lastname[10];
+  int id;
+  double gpa;
+};
+int main(){
+  cout << "student list" << endl;
+  return 0;
+}
+//add, print, delete
